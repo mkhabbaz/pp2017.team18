ebb9d4aa51b247f206ffdcc2550f59cf03d9266c
Konstantin Pilz <konstantin.pilz@smail.uni-koeln.de>
2017-07-03 18:34:26 +0200

    -Main Methode

---
+++ src/HindiBonesMain.java
@@ -0,0 +1,12 @@
+import gui.HindiBones;
+
+public class HindiBonesMain {
+
+	public static final int BOX = 32;
+	public static final int WIDTH = 16, HEIGHT = 16;
+	
+	public static void main(String[] args){
+		
+		new HindiBones(BOX*WIDTH, BOX*HEIGHT, "Hindi Bones");
+	}
+}
